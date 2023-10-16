e80380ae88a8a96eeb00d852938f2889a94123b8
Manahil-04 <manahilmushtaq004@gmail.com>
2023-10-16 15:15:47 +0500

    Add a file

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello World!")
+# print("Hello World!")
+
+class Book:
+    def __init__(self, book_id, title, author, available=True):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.available = available
+
+    def __str__(self):
+        return f"{self.title} by {self.author} (ID: {self.book_id})"
